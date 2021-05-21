da6e760aa0157bfaff0c91036d1f51aadc29440d
Martinez Lara Jose Eduardo <martinezlara_joseeduardo@hotmail.com>
2021-05-21 04:26:57 -0500

    libraries issues

--- essentials/libraries-issues.ts
+++ essentials/libraries-issues.ts
@@ -14,7 +14,8 @@ solution:
     npm install --save-dev @types/lodash
 
 in other cases where this package doesn't exists, you will have to use
-the keyword 'declare' to tell ts the variable/package exists
+the keyword 'declare' to tell ts the variable/package exists, in the root
+folder [src] create a file [decs.d.ts]
 example:
 
     declare module 'lodash'
